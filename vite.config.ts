844a4023518553faa35889a8d92197422ba7ea56
Meng Zhang <meng@tabbyml.com>
2025-10-09 14:27:12 -0700

    feat: configure Vite server to disable file watching

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    watch: null
+  },
   plugins: [
     tailwindcss(),
   ],
